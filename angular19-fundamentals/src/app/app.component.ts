03065ce3cdaa6c8b855b8a6ca9498be9ca13c313
Alexander Oliveira <alexander.oliveira99@gmail.com>
2025-05-28 19:54:40 +0100

    [Components, Core Directives and Pipes] - I was learning more about @Input on angular today

--- angular19-fundamentals/src/app/app.component.ts
+++ angular19-fundamentals/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CourseCardComponent } from "./course-card/course-card.component";
+import { COURSES } from './db-data';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,12 @@ import { CourseCardComponent } from "./course-card/course-card.component";
 export class AppComponent {
 
   title = 'Angular Fundamentals 19v';
+
+  coreCourse = COURSES[0];
+
+  rxjsCourse = COURSES[1];
+
+  ngrxCourse = COURSES[2];
 
   onLogoClick() {
     alert('Welcome new version angualar 19v')
--- angular19-fundamentals/src/app/course-card/course-card.component.ts
+++ angular19-fundamentals/src/app/course-card/course-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-course-card',
@@ -7,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './course-card.component.css'
 })
 export class CourseCardComponent {
+
+  @Input({
+    required: true
+  })
+  course!: Course;
+
+  constructor() { }
 
 }
